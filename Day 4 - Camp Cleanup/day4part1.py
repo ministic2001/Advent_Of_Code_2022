3b8be9ea3f495a764b268b168953725cdbd412b1
ministic2001 <kehxuan@gmail.com>
2022-12-09 00:12:03 +0800

    Updated Day 4 Challenge

---
+++ Day 4 - Camp Cleanup/day4part1.py
@@ -0,0 +1,53 @@
+import sys
+import os
+sys.path.insert(0, r"./file_reader")    #import file_reader function from file_reader/file_reader.py
+
+from file_reader import file_reader
+
+FILE: str = r"raw data.txt"
+CURRENT_WORKING_DIRECTORY: str = os.path.dirname(os.path.abspath(__file__))
+
+def convert_to_range_pair_array(raw_data: str) -> list[list[list[int, int], list[int, int]]]:
+    """
+    Converts the raw data into a range array. For example, given\n
+    13-53,17-82\n
+    32-32,32-42\n
+
+    This will be converted to [[[13, 53], [17, 82]], [[32, 32], [32, 42]]]
+
+    raw_data: The raw data from Advent Of Code 2022
+
+    return: A range array, example structure shown above
+    """
+    range_row_array: list[str] = raw_data.split("\n")
+    range_pair_array_unformatted: list[list[str, str]] = [range_row.split(",") for range_row in range_row_array]
+    range_pair_array_formatted: list[list[list[int, int], list[int, int]]] = [[list(map(int, range_row[0].split("-"))), list(map(int, range_row[1].split("-")))] for range_row in range_pair_array_unformatted]
+
+    return range_pair_array_formatted
+
+def check_if_inside(range_pair: list[list[int, int], list[int, int]]) -> bool:
+    """
+    Check if any pair range fully contain the other
+
+    range_pair: The pair of ranges to check
+
+    return: True or False
+    """
+    pair_range_first: list[str, str] = range_pair[0]
+    pair_range_second: list[str, str] = range_pair[1]
+
+    return (((pair_range_first[0] <= pair_range_second[0]) and (pair_range_first[1] >= pair_range_second[1])) or (pair_range_second[0] <= pair_range_first[0]) and (pair_range_second[1] >= pair_range_first[1]))
+        
+def main():
+    raw_data: str = file_reader(CURRENT_WORKING_DIRECTORY + r"/" + FILE)
+    range_pair_array: list[list[list[int, int], list[int, int]]] = convert_to_range_pair_array(raw_data)
+
+    pair_count: int = 0
+    for range_pair in range_pair_array:
+        if check_if_inside(range_pair):
+            pair_count += 1
+    
+    print(f"There are {pair_count} assignment pairs where one range fully contain the other")
+
+if __name__ == "__main__":
+    main()
